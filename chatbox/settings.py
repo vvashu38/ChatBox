b38c86a126dddb9a66195175461bdbda3fe7ff37
Subodh Vashistha <subodh@users.noreply.github.com>
2020-06-13 21:58:06 +0530

    register

--- chatbox/settings.py
+++ chatbox/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR,'register')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
--- login/urls.py
+++ login/urls.py
@@ -17,4 +17,5 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.login,name="login"),
+    path('register',views.register,name='register')
 ]
